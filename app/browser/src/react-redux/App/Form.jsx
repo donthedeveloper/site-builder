de0a2703c811d7b4f7c431c449d687a02e8f9c58
Jack <johnmccormick1118@gmail.com>
2018-12-06 09:42:16 -0600

    Changed SignupForm to be more flexible for error msgs.

--- app/browser/src/react-redux/App/Form.jsx
+++ app/browser/src/react-redux/App/Form.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 
 const Form = props => {
-  const regExReplace = str => {
-    let idx = str.search(/[A-Z]/g);
-    let newStr = str.replace(/^[a-zA-Z]/g, str => str.toUpperCase());
-    if (idx != -1) {
-      newStr = newStr.split("");
-      newStr.splice(idx, 0, " ");
-      newStr = newStr.join("");
-    }
-    return newStr;
-  };
   return (
     <div className="form">
-      <label className="form__label">{regExReplace(props.label)}</label>
+      <label className="form__label">
+        {props.label}
+        <span className="form__error">{props.error}</span>
+      </label>
       <input
-        type="text"
-        name={props.label}
-        onChange={e => props.handleChange(props.label, e.target.value)}
+        type={props.type}
+        name={props.name}
+        onChange={e => props.handleChange(props.name, e.target.value)}
         value={props.value}
         className="form__input"
       />
--- app/browser/src/react-redux/App/SignupForm.jsx
+++ app/browser/src/react-redux/App/SignupForm.jsx
@@ -8,7 +8,8 @@ class signupForm extends Component {
     this.state = {
       email: "",
       password: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      confirmPasswordError: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -18,20 +19,53 @@ class signupForm extends Component {
       [key]: e
     });
   }
+  //check if password and confirmPassword fields match. If not, set error message to state.
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.confirmPassword != this.state.confirmPassword &&
+      this.state.confirmPassword.length > 0 &&
+      this.state.password != this.state.confirmPassword
+    ) {
+      this.setState({ confirmPasswordError: "Passwords do not match." });
+    } else if (
+      prevState.confirmPassword !== this.state.confirmPassword &&
+      (this.state.confirmPassword === this.state.password ||
+        this.state.confirmPassword.length === 0)
+    ) {
+      this.setState({
+        confirmPasswordError: ""
+      });
+    }
+  }
   render() {
-    let inputs = Object.keys(this.state);
+    let name = Object.keys(this.state);
+    let mismatch = "Passwords do not match.";
     return (
       <div className="signup-form">
         <form action="" className="signup-form__form-group">
           <h1 className="signup-form__title">Signup for Site Builder</h1>
-          {inputs.map(input => (
-            <Form
-              label={input}
-              value={this.state[input]}
-              handleChange={this.handleChange}
-              key={input}
-            />
-          ))}
+          <Form
+            label="Email"
+            name={name[0]}
+            value={this.state[0]}
+            handleChange={this.handleChange}
+            type="email"
+          />
+          <Form
+            label="Password"
+            name={name[1]}
+            value={this.state[1]}
+            handleChange={this.handleChange}
+            type="password"
+          />
+          <Form
+            label="Confirm Password"
+            name={name[2]}
+            value={this.state[2]}
+            handleChange={this.handleChange}
+            error={this.state.error}
+            type="password"
+          />
           <div>
             <button className="signup-form__submit-button">Signup!</button>
           </div>
