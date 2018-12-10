fa47eff6814cfae5c2b7b0850b5f82c21ddc2ea7
Aurora Vaughn <avaughn.git@gmail.com>
2018-12-09 22:57:40 -0600

    Removed async / await test example"

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,14 +1,5 @@
-require('babel-polyfill');
-
-global.regeneratorRuntime = regeneratorRuntime;
-
 module.exports = {
-  // verbose: true,
-
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/.cache/']
-  // transformIgnorePatterns: ['/node_modules/(?!event-pubsub).+\\.js$'],
-  // transform: {
-  //   '^.+\\.js$': 'babel-jest'
-  // }
+
 };
--- server/routes/__test__/index.unit.test.js
+++ server/routes/__test__/index.unit.test.js
@@ -1,12 +1,17 @@
+require('@babel/polyfill'); // for async await
 const request = require('supertest');
 const app = require('../../../app');
 
 describe("Test the root path  '/'  ", () => {
-  test('The GET method', done => {
+  test('The GET method ', (done) => {
     return request(app)
       .get('/')
       .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(200, done)
+      .expect(200)
+      .end((err) => {
+        if (err) console.error(err)
+        done()
+      })
   })
 })
 
