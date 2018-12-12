c66af268235a95c14131224a1a76c782b06119d8
Matt Cool <matthewbcool@gmail.com>
2018-12-12 17:55:12 -0600

    moved form check to a seperate function and added some reg ex to email validation

--- browser/src/react-redux/App/Login/Login.jsx
+++ browser/src/react-redux/App/Login/Login.jsx
@@ -37,22 +37,27 @@ export default class Login extends React.Component {
         : this.setState({ validPassword: false })
     }
     //check if both email and password are valid
+    this.isValidatedCheck()
+  }
+
+  validateEmail(value) {
+    //just checks for an @ for now
+    const atCheck = /\S+@\S+/
+    return value.length >= 3 && atCheck.test(value) ? true : false
+  }
+  validatePassword(value) {
+    //just check length for now
+    return value.length >= 6 ? true : false
+  }
+
+  isValidatedCheck() {
     this.state.validEmail && this.state.validPassword
       ? this.setState({ isValidated: true })
       : this.setState({ isValidated: false })
   }
 
-  validateEmail(value) {
-    //insert email validation logic here
-    return value.length >= 3 ? true : false
-  }
-  validatePassword(value) {
-    //insert password validation logic here
-    return value.length >= 6 ? true : false
-  }
-
   displayErrors() {
-    //incoming massive if/else monster.
+    //incoming massive if/else statement- refactor.
     if (!this.state.validEmail && !this.state.validPassword) {
       this.setState({ emailError: 'Please enter a valid e-mail:' })
       this.setState({ passwordError: 'Please enter a valid password:' })
@@ -69,6 +74,7 @@ export default class Login extends React.Component {
 
   handleSubmit(e) {
     this.displayErrors()
+    console.log('submitted')
     e.preventDefault()
   }
 
@@ -81,6 +87,7 @@ export default class Login extends React.Component {
           name='email'
           label='email'
           type='email'
+          placeholder='email address'
           value={this.state.email}
           onChange={this.handleInputChange}
         />
