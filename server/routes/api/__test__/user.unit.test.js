5e97005092e8ab40839721f6d50334867c2450c3
benjaminsinaiko <ben.jamin@outlook.com>
2019-01-08 14:38:14 -0600

    Test user/post for empty payload error

--- server/routes/api/__test__/user.unit.test.js
+++ server/routes/api/__test__/user.unit.test.js
@@ -1,7 +1,20 @@
 const request = require('supertest');
 const app = require('../../../../app');
+
 describe('User Routes', () => {
   test('GET method', done => {
     done();
+  });
+
+  test('POST method with no payload', () => {
+    return request(app)
+      .post('/api/user')
+      .set('Accept', 'application/json')
+      .then(res => {
+        expect(res.statusCode).toBe(400);
+        expect(res.body.error.errors.email.message).toBe(
+          'You must provide an email address.'
+        );
+      });
   });
 });
