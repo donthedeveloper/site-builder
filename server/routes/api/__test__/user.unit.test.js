5c1580341ffb4088dc12b9010b942df67e1f4a4f
benjaminsinaiko <ben.jamin@outlook.com>
2019-01-10 20:59:28 -0600

    Add tests for user auth no email, no password

--- server/routes/api/__test__/user.unit.test.js
+++ server/routes/api/__test__/user.unit.test.js
@@ -15,6 +15,7 @@ describe('User Routes', () => {
         .type('form')
         .then(res => {
           expect(res.statusCode).toEqual(400);
+          expect(Object.keys(res.body.error.errors).length).toEqual(2);
           expect(Object.keys(res.body.error.errors)).toEqual(
             expect.arrayContaining(['email', 'password'])
           );
@@ -27,30 +28,38 @@ describe('User Routes', () => {
         });
     });
 
-    //   test('POST method with no email', () => {
-    //     return request(app)
-    //       .post('/api/user')
-    //       .send('password=test')
-    //       .set('Accept', 'application/json')
-    //       .then(res => {
-    //         expect(res.statusCode).toBe(400);
-    //         expect(res.body.error.errors.email.message).toBe(
-    //           'You must provide an email address.'
-    //         );
-    //       });
-    //   });
+    test('POST method with no email', () => {
+      return request(app)
+        .post('/api/user')
+        .type('form')
+        .send('password=test')
+        .then(res => {
+          expect(res.statusCode).toEqual(400);
+          expect(Object.keys(res.body.error.errors).length).toEqual(1);
+          expect(Object.keys(res.body.error.errors)).toEqual(
+            expect.arrayContaining(['email'])
+          );
+          expect(res.body.error.errors.email.message).toBe(
+            'You must provide an email address.'
+          );
+        });
+    });
 
-    //   test('POST method with no password', () => {
-    //     return request(app)
-    //       .post('/api/user')
-    //       .send('email=test@test.com')
-    //       .set('Accept', 'application/json')
-    //       .then(res => {
-    //         expect(res.statusCode).toBe(400);
-    //         expect(res.body.error.errors.password.message).toBe(
-    //           'You must provide a password.'
-    //         );
-    //       });
-    //   });
+    test('POST method with no password', () => {
+      return request(app)
+        .post('/api/user')
+        .type('form')
+        .send('email=example@test.com')
+        .then(res => {
+          expect(res.statusCode).toEqual(400);
+          expect(Object.keys(res.body.error.errors).length).toEqual(1);
+          expect(Object.keys(res.body.error.errors)).toEqual(
+            expect.arrayContaining(['password'])
+          );
+          expect(res.body.error.errors.password.message).toBe(
+            'You must provide a password.'
+          );
+        });
+    });
   });
 });
