444e21a2d59142e96bc5510365be87fa5fc31e16
benjaminsinaiko <ben.jamin@outlook.com>
2019-01-23 14:21:47 -0600

    Add user auth test file

---
+++ server/routes/api/__test__/auth.unit.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../../../../app');
+const User = require('../../../models/user');
+
+describe('Auth Routes', () => {
+  test('Auth routes here', done => {
+    done();
+  });
+});
