d415eb4e820362217dd6a0e1e9fdc75746c739ed
benjaminsinaiko <ben.jamin@outlook.com>
2019-01-24 19:07:45 -0600

    Add test for invalid email, update authenticate method error message  in model

--- server/routes/api/__test__/auth.unit.test.js
+++ server/routes/api/__test__/auth.unit.test.js
@@ -6,4 +6,17 @@ describe('Auth Routes', () => {
   test('Auth routes here', done => {
     done();
   });
+
+  test('Invalid email should return error', () => {
+    return request(app)
+      .post('/api/auth/login')
+      .type('form')
+      .send('email=test@test')
+      .then(res => {
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.error.message).toBe(
+          'Incorrect email and password combination.'
+        );
+      });
+  });
 });
