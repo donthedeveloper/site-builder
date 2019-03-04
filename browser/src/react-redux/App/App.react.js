b163d2feb6e16e257010a5225b760313667e6385
benjaminsinaiko <ben.jamin@outlook.com>
2019-03-04 17:14:46 -0600

    Removed Private route from signup, kept redirect if user exists

--- browser/src/react-redux/App/App.react.js
+++ browser/src/react-redux/App/App.react.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import PrivateRoute from './PrivateRoute.react'
 import SignupPage from './SignupPage/SignupPage.react'
 import Nav from './Nav.react'
 import LoginPage from './LoginPage/LoginPage.react'
@@ -9,7 +8,7 @@ export default props => (
   <div>
     <Nav />
     <Switch>
-      <PrivateRoute path="/signup" component={SignupPage} />
+      <Route path="/signup" component={SignupPage} />
       <Route path="/login" component={LoginPage} />
     </Switch>
   </div>
