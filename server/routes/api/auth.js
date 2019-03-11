e0c5a621a56b1cd0a1c2e8a7b394c7022a970145
benjaminsinaiko <ben.jamin@outlook.com>
2019-03-11 17:09:23 -0500

    Add mailer after password is reset for confirmation, return user object on success

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -36,8 +36,6 @@ router.post('/forgot', async (req, res) => {
   user.resetPassword.token = token;
   user.resetPassword.expiration = Date.now() + 3600000;
   try {
-    console.log(user.resetPassword.token); // Reset token
-
     await user.save();
   } catch (err) {
     return res.status(500).json({ error: err });
@@ -74,15 +72,38 @@ router.get('/reset/:token', async (req, res) => {
     return res.status(401).json('Invalid or expired token.').end();
   }
 
+  // reset password
   user.password = req.body.password;
   user.resetPassword.token = undefined;
   user.resetPassword.expiration = undefined;
 
   try {
     await user.save();
-    return res.status(200).end();
   } catch (err) {
     return res.status(500).json({ error: err });
+  }
+
+  const smtpTransport = nodemailer.createTransport({
+    host: 'smtp.sendgrid.net',
+    port: 587,
+    auth: {
+      user: 'apikey',
+      pass: process.env.SG_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.PASSWORD_RESET_AUTH_EMAIL,
+    to: user.email,
+    subject: 'Your password has been changed',
+    html: `<p>The password for ${user.email} has been changed.</p>`,
+  };
+
+  try {
+    await smtpTransport.sendMail(mailOptions);
+    return res.status(200).json(user).end();
+  } catch (err) {
+    return res.status(500).end();
   }
 });
 
