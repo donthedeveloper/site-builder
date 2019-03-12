2a4355924b962780215491ce64ee91a4442c7150
Jack McCormick <johnmccormick1118@gmail.com>
2019-03-12 13:49:51 -0500

    Fixed routing error.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const apiRouter = require('./api');
 
 const router = express.Router();
 
 router.use('/api', apiRouter);
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../browser/dist/index.html'));
+});
+
 
 module.exports = router;
