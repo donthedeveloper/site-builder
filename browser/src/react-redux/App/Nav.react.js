6f35a0344b9e223e28104c800a75bd1d7dffc34e
benjaminsinaiko <ben.jamin@outlook.com>
2019-03-14 12:39:06 -0500

    Add prop type validation for nav bar user prop

--- browser/src/react-redux/App/Nav.react.js
+++ browser/src/react-redux/App/Nav.react.js
@@ -1,32 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Logout from './Logout/Logout.React';
 
-const User = props => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      {!props.user ? (
-        <>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          {' '}
-        </>
-      ) : (
+const User = (props) => {
+  const { user } = props;
+  return (
+    <nav>
+      <ul>
         <li>
+          <Link to="/">Home</Link>
+        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            {' '}
+          </>
+        ) : (
+          <li>
             <Logout />
           </li>
-      )}
-    </ul>
-  </nav>
-);
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+User.defaultProps = {
+  user: null,
+};
+
+User.propTypes = {
+  user: PropTypes.shape({
+    _id: PropTypes.string,
+    email: PropTypes.string,
+    createdAt: PropTypes.string,
+    updatedAt: PropTypes.string,
+    __v: PropTypes.number,
+  }),
+};
 
 const mapStateToProps = state => ({ user: state.user });
 
