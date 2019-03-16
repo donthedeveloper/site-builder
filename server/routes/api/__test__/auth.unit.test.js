4428b971af7d466f087e6b7cce795607ce9c0216
Jack McCormick <johnmccormick1118@gmail.com>
2019-03-15 20:46:50 -0500

    Added test block comments and a small fix.

--- server/routes/api/__test__/auth.unit.test.js
+++ server/routes/api/__test__/auth.unit.test.js
@@ -52,33 +52,30 @@ describe('POST Auth/Login', () => {
 describe('POST Auth/Forgot', () => {
   const existingUserEmail = 'testUser@test.com';
 
-  beforeAll(() => {
-    return User.create({ email: existingUserEmail, password: 'test' });
-  });
+  beforeAll(() => User.create({ email: existingUserEmail, password: 'test' }));
 
-  afterAll(() => {
-    return User.findOneAndDelete({ email: existingUserEmail });
-  });
+  afterAll(() => User.findOneAndDelete({ email: existingUserEmail }));
   it('will return status 200 on nonexistent user', async () => {
     const res = await request(app)
       .post('/api/auth/forgot')
       .type('form')
       .send({ email: 'doesnotexist@test.com' });
     expect(res.statusCode).toEqual(200);
-  });
+  }); // send 200 on non existent user email instead of error for security purposes.
   it('will return status 200 on existing user', async () => {
     const res = await request(app)
       .post('/api/auth/forgot')
       .type('form')
       .send({ email: existingUserEmail });
     expect(res.statusCode).toEqual(200);
-  });
-  it('will create, then save a token and expiration to user', async () => {
+  }); // send 200 on existent user email.
+  it('will create and then save a token and expiration to user', async () => {
     await request(app)
       .post('/api/auth/forgot')
       .type('form')
       .send({ email: existingUserEmail });
     const user = await User.findOne({ email: existingUserEmail }).exec();
-    expect(user.resetPassword.token && user.resetPassword.expiration);
-  });
+    expect(user.resetPassword.token && user.resetPassword.expiration).toBeTruthy();
+  }); /* token and expiration need to be assigned to
+      a user then sent via email to verify reset password. */
 });
