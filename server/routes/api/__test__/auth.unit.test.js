2ffd1ed10f63c473eec42a51213365fe6618729b
Jack McCormick <johnmccormick1118@gmail.com>
2019-03-23 11:16:42 -0500

    Made changes to auth.unit.test.js

--- server/routes/api/__test__/auth.unit.test.js
+++ server/routes/api/__test__/auth.unit.test.js
@@ -55,20 +55,24 @@ describe('POST Auth/Forgot', () => {
   beforeAll(() => User.create({ email: existingUserEmail, password: 'test' }));
 
   afterAll(() => User.findOneAndDelete({ email: existingUserEmail }));
+
+  // send 200 on non existent user email instead of error for security purposes.
   it('will return status 200 on nonexistent user', async () => {
     const res = await request(app)
       .post('/api/auth/forgot')
       .type('form')
       .send({ email: 'doesnotexist@test.com' });
     expect(res.statusCode).toEqual(200);
-  }); // send 200 on non existent user email instead of error for security purposes.
+  });
+
   it('will return status 200 on existing user', async () => {
     const res = await request(app)
       .post('/api/auth/forgot')
       .type('form')
       .send({ email: existingUserEmail });
     expect(res.statusCode).toEqual(200);
-  }); // send 200 on existent user email.
+  });
+
   it('will create and then save a token and expiration to user', async () => {
     await request(app)
       .post('/api/auth/forgot')
@@ -76,6 +80,5 @@ describe('POST Auth/Forgot', () => {
       .send({ email: existingUserEmail });
     const user = await User.findOne({ email: existingUserEmail }).exec();
     expect(user.resetPassword.token && user.resetPassword.expiration).toBeTruthy();
-  }); /* token and expiration need to be assigned to
-      a user then sent via email to verify reset password. */
+  });
 });
