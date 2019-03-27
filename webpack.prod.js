335dcfd743222a79876cdd5e793b7a866596298c
Jack McCormick <johnmccormick1118@gmail.com>
2019-03-27 09:57:53 -0500

    reverted master merge in.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,7 @@
-const path = require('path');
 const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackTemplate = require('html-webpack-template');
+
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'browser/dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      appMountId: 'app',
-      template: HtmlWebpackTemplate,
-    }),
-  ],
 });
